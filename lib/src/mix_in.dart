9230c46c3985ed38dfef883bb677ffd16917d0e6
Thomas Burkhart <github@burkharts.net>
2020-09-25 17:28:57 +0200

    added state mixin

--- lib/src/mix_in.dart
+++ lib/src/mix_in.dart
@@ -6,10 +6,15 @@ import 'package:flutter/src/widgets/async.dart';
 import 'package:flutter/widgets.dart';
 import 'package:get_it/get_it.dart';
 
+class _MutableWrapper<T> {
+  T value;
+}
+
 mixin GetItMixin on StatelessWidget {
-  final _MixinState _state = _MixinState();
+  /// this is an ugly hack so that you don't get a warning in the StatelessWidget
+  final _MutableWrapper<_MixinState> _state = _MutableWrapper<_MixinState>();
   @override
-  StatelessElement createElement() => _StatelessMixInElement(this, _state);
+  StatelessElement createElement() => _StatelessMixInElement(this);
 
   /// all the following functions can be called inside the build function but also
   /// in e.g. in `initState` of a `StatefulWidget`.
@@ -37,7 +42,7 @@ mixin GetItMixin on StatelessWidget {
   /// like [get] but it also registers a listener to [T] and
   /// triggers a rebuild every time [T] signals a change
   T watch<T extends Listenable>({String instanceName}) =>
-      _state.watch<T>(instanceName: instanceName);
+      _state.value.watch<T>(instanceName: instanceName);
 
   /// like [get] but it also registers a listener to the result of [select] and
   /// triggers a rebuild every time signals [R] a change
@@ -46,7 +51,7 @@ mixin GetItMixin on StatelessWidget {
     R Function(T) select, {
     String instanceName,
   }) =>
-      _state.watchX<T, R>(select, instanceName: instanceName);
+      _state.value.watchX<T, R>(select, instanceName: instanceName);
 
   /// like watch but it only triggers a rebuild when the value that the function
   /// [only] returns changes. With that you can react to changes of single members
@@ -55,7 +60,7 @@ mixin GetItMixin on StatelessWidget {
     R Function(T) only, {
     String instanceName,
   }) =>
-      _state.watchOnly<T, R>(only, instanceName: instanceName);
+      _state.value.watchOnly<T, R>(only, instanceName: instanceName);
 
   /// a combination of [watchX] and [watchOnly]
   R watchXOnly<T, Q extends Listenable, R>(
@@ -63,7 +68,8 @@ mixin GetItMixin on StatelessWidget {
     R Function(Q) only, {
     String instanceName,
   }) =>
-      _state.watchXOnly<T, Q, R>(select, only, instanceName: instanceName);
+      _state.value
+          .watchXOnly<T, Q, R>(select, only, instanceName: instanceName);
 
   /// subscribes to the `Stream` returned by [select] and returns
   /// an `AsyncSnapshot` with the latest received data from the `Stream`
@@ -82,7 +88,7 @@ mixin GetItMixin on StatelessWidget {
     String instanceName,
     bool preserveState = true,
   }) =>
-      _state.watchStream<T, R>(select, initialValue,
+      _state.value.watchStream<T, R>(select, initialValue,
           instanceName: instanceName, preserveState: preserveState);
 
   /// awaits the ` Future` returned by [select] and triggers a rebuild as soon
@@ -104,7 +110,7 @@ mixin GetItMixin on StatelessWidget {
     String instanceName,
     bool preserveState = true,
   }) =>
-      _state.watchFuture<T, R>(select, initialValue,
+      _state.value.watchFuture<T, R>(select, initialValue,
           instanceName: instanceName, preserveState: preserveState);
 
   /// Pushes a new GetIt-Scope. After pushing it executes [init] where you can register
@@ -117,15 +123,165 @@ mixin GetItMixin on StatelessWidget {
   /// I would recommend doing pushing and popping from your business layer but sometimes
   /// this might come in handy
   void pushScope({void Function(GetIt getIt) init, void Function() dispose}) =>
-      _state.pushScope(init: init, dispose: dispose);
+      _state.value.pushScope(init: init, dispose: dispose);
 }
 
-class _StatelessMixInElement extends StatelessElement with _GetItElement {
-  _StatelessMixInElement(Widget widget, _MixinState state) : super(widget) {
-    _state = state;
+class _StatelessMixInElement<W extends GetItMixin> extends StatelessElement
+    with _GetItElement {
+  _StatelessMixInElement(
+    W widget,
+  ) : super(widget) {
+    _state = _MixinState();
+    widget._state.value = _state;
+  }
+  @override
+  W get widget => super.widget;
+
+  @override
+  void update(Widget newWidget) {
+    super.update(newWidget);
+    widget._state.value = _state;
   }
 }
 
+mixin GetItStateMixin on State {
+  /// this is an ugly hack so that you don't get a warning in the statelesswidget
+  final _MutableWrapper<_MixinState> _state = _MutableWrapper<_MixinState>();
+  @override
+  void initState() {
+    _state.value = _MixinState();
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    _state.value.init(context);
+    super.didChangeDependencies();
+  }
+
+  @override
+  void didUpdateWidget(Widget newWidget) {
+    _state.value.clearRegistratons();
+    _state.value.resetCurrentWatch();
+    super.didUpdateWidget(newWidget);
+  }
+
+  @override
+  void dispose() {
+    _state.value.dispose();
+    super.dispose();
+  }
+
+  /// all the following functions can be called inside the build function but also
+  /// the mixin takes care that everything is correctly disposed.
+
+  /// retrieves or creates an instance of a registered type [t] depending on the registration
+  /// function used for this type or based on a name.
+  /// for factories you can pass up to 2 parameters [param1,param2] they have to match the types
+  /// given at registration with [registerfactoryparam()]
+  T get<T>({String instanceName, dynamic param1, dynamic param2}) =>
+      GetIt.I<T>(instanceName: instanceName, param1: param1, param2: param2);
+
+  /// like [get] but for async registrations
+  Future<T> getasync<T>(
+          {String instanceName, dynamic param1, dynamic param2}) =>
+      GetIt.I.getAsync<T>(
+          instanceName: instanceName, param1: param1, param2: param2);
+
+  /// like [get] but with an additional [select] function to return a member of [T]
+  R getx<T, R>(R Function(T) accessor, {String instanceName}) {
+    assert(accessor != null);
+    return accessor(GetIt.I<T>(instanceName: instanceName));
+  }
+
+  /// like [get] but it also registers a listener to [T] and
+  /// triggers a rebuild every time [T] signals a change
+  T watch<T extends Listenable>({String instanceName}) =>
+      _state.value.watch<T>(instanceName: instanceName);
+
+  /// like [get] but it also registers a listener to the result of [select] and
+  /// triggers a rebuild every time signals [R] a change
+  /// useful if the `listenable` [R] is a member of your business object [T]
+  R watchX<T, R extends Listenable>(
+    R Function(T) select, {
+    String instanceName,
+  }) =>
+      _state.value.watchX<T, R>(select, instanceName: instanceName);
+
+  /// like watch but it only triggers a rebuild when the value that the function
+  /// [only] returns changes. with that you can react to changes of single members
+  /// of [T]
+  R watchOnly<T extends Listenable, R>(
+    R Function(T) only, {
+    String instanceName,
+  }) =>
+      _state.value.watchOnly<T, R>(only, instanceName: instanceName);
+
+  /// a combination of [watchx] and [watchonly]
+  R watchXonly<T, Q extends Listenable, R>(
+    Q Function(T) select,
+    R Function(Q) only, {
+    String instanceName,
+  }) =>
+      _state.value
+          .watchXOnly<T, Q, R>(select, only, instanceName: instanceName);
+
+  /// subscribes to the `Stream` returned by [select] and returns
+  /// an `AsyncSnapshot` with the latest received data from the `Stream`
+  /// Whenever new data is received it triggers a rebuild.
+  /// When you call [watchStream] a second time on the same `Stream` it will
+  /// return the last received data but not subscribe another time.
+  /// To be able to use [watchStream] inside a `build` function we have to pass
+  /// [initialValue] so that it can return something before it has received the first data
+  /// if [select] returns a different Stream than on the last call, [watchStream]
+  /// will cancel the previous subscription and subscribe to the new stream.
+  /// [preserveState] determines then if the new initial value should be the last
+  /// value of the previous stream or again [initialValue]
+  AsyncSnapshot<R> watchStream<T, R>(
+    Stream<R> Function(T) select,
+    R initialValue, {
+    String instanceName,
+    bool preserveState = true,
+  }) =>
+      _state.value.watchStream<T, R>(select, initialValue,
+          instanceName: instanceName, preserveState: preserveState);
+
+  /// awaits the ` Future` returned by [select] and triggers a rebuild as soon
+  /// as the `Future` completes. After that it returns
+  /// an `AsyncSnapshot` with the received data from the `Future`
+  /// When you call [watchFuture] a second time on the same `Future` it will
+  /// return the last received data but not observe the Future a another time.
+  /// To be able to use [watchStream] inside a `build` function
+  /// we have to pass [initialValue] so that it can return something before
+  /// the `Future` has completed
+  /// if [select] returns a different `Future` than on the last call, [watchFuture]
+  /// will ignore the completion of the previous Future and observe the completion
+  /// of the new Future.
+  /// [preserveState] determines then if the new initial value should be the last
+  /// value of the previous stream or again [initialValue]
+  AsyncSnapshot<R> watchFuture<T, R>(
+    Future<R> Function(T) select,
+    R initialValue, {
+    String instanceName,
+    bool preserveState = true,
+  }) =>
+      _state.value.watchFuture<T, R>(select, initialValue,
+          instanceName: instanceName, preserveState: preserveState);
+
+  /// Pushes a new GetIt-Scope. After pushing it executes [init] where you can register
+  /// objects that should only exist as long as this scope exists.
+  /// Can be called inside the `build` method method of a `StatelessWidget`.
+  /// It ensures that it's only called once in the lifetime of a widget.
+  /// When the widget is destroyed the scope too gets destroyed after [dispose]
+  /// is executed. If you use this function and you have registered your objects with
+  /// an async disposal function, that functions won't be awaited.
+  /// I would recommend doing pushing and popping from your business layer but sometimes
+  /// this might come in handy
+  void pushScope({void Function(GetIt getIt) init, void Function() dispose}) =>
+      _state.value.pushScope(init: init, dispose: dispose);
+}
+
+/// will cancel the previous subscription and subscribe to the new stream.
 mixin _GetItElement on ComponentElement {
   _MixinState _state;
 
@@ -159,14 +315,14 @@ mixin _GetItElement on ComponentElement {
   }
 }
 
-class _WatchEntry<T> extends LinkedListEntry<_WatchEntry<T>> {
+class _WatchEntry extends LinkedListEntry<_WatchEntry> {
   Listenable currentListenable;
   Function notificationHandler;
   StreamSubscription subscription;
   Stream currentStream;
   Future currentFuture;
   void Function(_WatchEntry entry) _dispose;
-  T lastValue;
+  dynamic lastValue;
   _WatchEntry(
       {this.notificationHandler,
       this.subscription,
@@ -195,11 +351,11 @@ class _MixinState {
     currentWatch = watchList.isNotEmpty ? watchList.first : null;
   }
 
-  _WatchEntry<T> _getWatch<T>() {
+  _WatchEntry _getWatch<T>() {
     if (currentWatch != null) {
       final result = currentWatch;
       currentWatch = currentWatch.next;
-      return result as _WatchEntry<T>;
+      return result;
     }
     return null;
   }
@@ -215,7 +371,7 @@ class _MixinState {
 
     if (watch == null) {
       final handler = () => _element.markNeedsBuild();
-      _appendWatch(_WatchEntry<T>(
+      _appendWatch(_WatchEntry(
         currentListenable: listenable,
         lastValue: listenable,
         notificationHandler: handler,
@@ -233,24 +389,25 @@ class _MixinState {
     assert(select != null, 'select can\'t be null if you use watchX');
     final parentObject = GetIt.I<T>(instanceName: instanceName);
     final listenable = select(parentObject);
-    assert(listenable == null, 'select returned null in watchX');
+    assert(listenable != null, 'select returned null in watchX');
 
-    _WatchEntry<R> watch = _getWatch();
+    _WatchEntry watch = _getWatch();
 
     if (watch != null) {
-      if (select(parentObject) == watch.currentListenable) {
-        return watch.lastValue;
+      if (listenable == watch.currentListenable) {
+        return listenable;
       } else {
         /// select returned a different value than the last time
         /// so we have to unregister out handler and subscribe anew
         watch.dispose();
       }
     } else {
-      _appendWatch(_WatchEntry<R>(
+      watch = _WatchEntry(
         dispose: (x) => listenable.removeListener(
           x.notificationHandler,
         ),
-      ));
+      );
+      _appendWatch(watch);
     }
 
     final handler = () => _element.markNeedsBuild();
@@ -268,10 +425,10 @@ class _MixinState {
     assert(only != null, 'only can\'t be null if you use watchOnly');
     final parentObject = GetIt.I<T>(instanceName: instanceName);
 
-    _WatchEntry<R> alreadyRegistered = _getWatch();
+    _WatchEntry alreadyRegistered = _getWatch();
 
     if (alreadyRegistered == null) {
-      final watch = _WatchEntry<R>(
+      final watch = _WatchEntry(
           currentListenable: parentObject,
           lastValue: only(parentObject),
           dispose: (x) => parentObject.removeListener(x.notificationHandler));
@@ -302,7 +459,7 @@ class _MixinState {
     final Q listenable = select(parentObject);
     assert(listenable != null, 'watchXOnly: select must return a Listenable');
 
-    _WatchEntry<R> watch = _getWatch();
+    _WatchEntry watch = _getWatch();
 
     if (watch != null) {
       if (listenable == watch.currentListenable) {
@@ -313,7 +470,7 @@ class _MixinState {
         watch.dispose();
       }
     } else {
-      watch = _WatchEntry<R>(
+      watch = _WatchEntry(
           lastValue: only(listenable),
           dispose: (x) => listenable.removeListener(x.notificationHandler));
       _appendWatch(watch);
@@ -345,7 +502,7 @@ class _MixinState {
     final stream = select(parentObject);
     assert(stream != null, 'select returned null in watchX');
 
-    _WatchEntry<AsyncSnapshot<R>> watch = _getWatch();
+    _WatchEntry watch = _getWatch();
 
     if (watch != null) {
       if (stream == watch.currentStream) {
@@ -388,14 +545,14 @@ class _MixinState {
       {String instanceName, bool preserveState}) {
     assert(select != null, 'select can\'t be null if you use watchStream');
     final parentObject = GetIt.I<T>(instanceName: instanceName);
-    final future = select(parentObject);
-    assert(future != null, 'select returned null in watchX');
+    final Future = select(parentObject);
+    assert(Future != null, 'select returned null in watchX');
 
-    _WatchEntry<AsyncSnapshot<R>> watch = _getWatch();
+    _WatchEntry watch = _getWatch();
 
     if (watch != null) {
-      if (future == watch.currentFuture) {
-        ///  still the same future so we can directly return lastvalue
+      if (Future == watch.currentFuture) {
+        ///  still the same Future so we can directly return lastvalue
         return watch.lastValue;
       } else {
         /// select returned a different value than the last time
@@ -409,7 +566,7 @@ class _MixinState {
       _appendWatch(watch);
     }
 
-    watch.currentFuture = future.then(
+    watch.currentFuture = Future.then(
       (x) {
         if (watch.currentFuture != null) {
           // only update if Future is still valid
